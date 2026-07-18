1b0e837c4814683f0dd5b53c2aa337ebbcb25895
lexuanhuy <lexuanhuy1994@gmail.com>
2026-07-18 09:37:42 +0700

    feat: add new cards effect

--- new-effect.js
+++ new-effect.js
@@ -26,11 +26,11 @@ if (exists.stdout.trim() === 'True') {
 console.log(chalk.blue(`Đang tạo ${path}...`));
 
 await $`mkdir -p ${path}`;
-writeFileSync(`${path}/style.css`, '');
+writeFileSync(`${path}/style.css`, ':root { --bg-color: #0f172a; --accent-color: #38bdf8; } * { box-sizing: border-box; } body { background-color: var(--bg-color); display: flex; } .effect-wrapper { margin: auto; }');
 console.log(chalk.green(`Đã tạo xong style.css!`));
 writeFileSync(`${path}/meta.json`, JSON.stringify(metaContent, null, 2));
 console.log(chalk.green(`Đã tạo xong meta.json!`));
-writeFileSync(`${path}/index.html`, '<link rel="stylesheet" href="style.css" />');
+writeFileSync(`${path}/index.html`, '<link rel="stylesheet" href="style.css" />\n<div class="effect-wrapper"></div>');
 console.log(chalk.green(`Đã tạo xong index.html!`));
 writeFileSync(`${path}/main.js`, '');
 console.log(chalk.green(`Đã tạo xong main.js!`));
